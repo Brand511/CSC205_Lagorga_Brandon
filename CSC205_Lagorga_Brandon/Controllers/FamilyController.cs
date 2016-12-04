6132e05511caa39f9725764b5e3aa579c3b32c5d
Brand511 <bllagorg@geneva.edu>
2016-12-04 14:12:29 -0500

    Finaly done, Final Project
    I used some of Even's Wehr coding to help out with the editing and etc,
    for the people and Family

---
+++ CSC205_Lagorga_Brandon/Controllers/FamilyController.cs
@@ -0,0 +1,174 @@
+﻿using CSC205_Lagorga_Brandon.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CSC205_Lagorga_Brandon.Controllers
+{
+    public class FamilyController : Controller
+    {
+
+        List<Family> families;
+
+        public FamilyController()
+        {
+            families = new List<Family>
+            {
+                new Family() { id=0, familyname = "Madeira", address1 = "123 Hastings Dr", city = "Cranberry Township", state = "PA", zip = "16066", homephone = "7247797964" },
+                new Family() { id=1, familyname = "Johns", address1 = "3200 College Ave", city = "Beaver Falls", state = "PA", zip = "15010", homephone = "7248461298" },
+                new Family() { id=2, familyname = "Ellis", address1 = "1 Sycamore Hollow", city = "Pittsburgh", state = "PA", zip = "15212", homephone = "4122371212" },
+                new Family() { id=3, familyname = "Braddock", address1 = "23 Livingstone Dr", city = "Monroeville", state = "PA", zip = "15010", homephone = "4123277486" }
+            };
+
+
+        }
+        protected override void Initialize(RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+            if (Session["familyList"] == null)
+            {
+                Session["familyList"] = families;
+            }
+        }
+        // GET: Family
+        public ActionResult Index()
+        {
+            var f = (List<Family>)Session["FamilyList"];
+            return View(f);
+        }
+
+        // GET: Family/Details/5
+        public ActionResult Details(int id)
+        {
+            var fList = (List<Family>)Session["familyList"];
+            var f = fList[id];
+
+            return View(f);
+        }
+
+        // GET: Family/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Family/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                families = (List<Family>)Session["familyList"];
+                Family newFamily = new Family()
+                {
+                    id = families.Count(),
+                    familyname = collection["familyname"],
+                    address1 = collection["middlename"],
+                    city = collection["lastname"],
+                    state = collection["cell"],
+                    zip = collection["relationship"],
+                    homephone = collection["homephone"]
+
+                };
+
+                families = (List<Family>)Session["familyList"];
+                families.Add(newFamily);
+                Session["familyList"] = families;
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Family/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var fList = (List<Family>)Session["familyList"];
+            var f = fList[id];
+            //var f = families[id];
+
+            return View(f);
+        }
+
+        // POST: Family/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                var fList = (List<Family>)Session["familyList"];
+
+
+                var f = fList[id];
+
+                Family newFamily = new Family()
+                {
+                    id = id,
+                    familyname = collection["familyname"],
+                    address1 = collection["address1"],
+                    city = collection["city"],
+                    state = collection["state"],
+                    zip = collection["zip"],
+                    homephone = collection["homephone"]
+
+                };
+                fList.Where(x => x.id == id).First().familyname = collection["familyname"];
+                fList.Where(x => x.id == id).First().address1 = collection["address1"];
+                fList.Where(x => x.id == id).First().city = collection["city"];
+                fList.Where(x => x.id == id).First().state = collection["state"];
+                fList.Where(x => x.id == id).First().zip = collection["zip"];
+                fList.Where(x => x.id == id).First().homephone = collection["homephone"];
+                //Session["peopleList"] = pList.Where(x => x.id != id).ToList();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Family/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var fList = (List<Family>)Session["familyList"];
+            var f = fList[id];
+            return View(f);
+        }
+
+        // POST: Family/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                var fList = (List<Family>)Session["familyList"];
+
+
+                var f = fList[id];
+
+                Session["familyList"] = fList.Where(x => x.id != id).ToList();
+                fList = (List<Family>)Session["familyList"];
+                for (int x = id; x < fList.Count(); x++)
+                {
+                    if (fList[x] != null)
+                        fList[x].id = x;
+                }
+
+
+
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ CSC205_Lagorga_Brandon/Controllers/PersonController.cs
@@ -0,0 +1,171 @@
+﻿using CSC205_Lagorga_Brandon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace CSC205_Lagorga_Brandon.Controllers
+{
+    public class PersonalController : Controller
+    {
+        new public List<Person> people;
+
+
+        public PersonalController()
+        {
+            people = new List<Person>
+            {
+                new Person() { id=0, firstname = "G", middlename = "Scott", lastname = "Madeira", cell = "4124180163", relationship = "dad", familyId = 0 },
+                new Person() { id=1, firstname="Jean" , middlename="E" , lastname="Madeira" , cell="4125551122" , relationship="mom" , familyId=0 },
+                new Person() { id=2, firstname="Nick" , middlename="A" , lastname="Madeira" , cell="4125559988" , relationship="son" , familyId=0 },
+                new Person() { id=3, firstname="John" , middlename="M" , lastname="Madeira" , cell="4125551234" , relationship="son" , familyId=0 },
+                new Person() { id=4, firstname="Chris" , middlename="T" , lastname="Madeira" , cell="4125556758" , relationship="son" , familyId=0 },
+                new Person() { id=5, firstname="Jimmy" , middlename="J" , lastname="Johns" , cell="6075556758" , relationship="dad" , familyId=1 },
+                new Person() { id=6, firstname="Stacey" , middlename="H" , lastname="Johns" , cell="6075556757" , relationship="mom" , familyId=1 },
+                new Person() { id=7, firstname="Ian" , middlename="F" , lastname="Johns" , cell="6075552257" , relationship="son" , familyId=1 },
+                new Person() { id=8, firstname="Avery" , middlename="K" , lastname="Johns" , cell="6075534757" , relationship="daughter" , familyId=1 },
+                new Person() { id=9, firstname="Roy" , middlename="F" , lastname="Ellis" , cell="9035534757" , relationship="dad" , familyId=2 },
+                new Person() { id=10, firstname="Michelle" , middlename="" , lastname="Ellis" , cell="9035531947" , relationship="mom" , familyId=2 },
+                new Person() { id=11, firstname="Bernie" , middlename="S" , lastname="Braddock" , cell="8145534757" , relationship="mom" , familyId=3 },
+                new Person() { id=12, firstname="Mark" , middlename="P" , lastname="Anderson" , cell="3025534757" , relationship="son" , familyId=3 }
+            };
+
+        }
+        protected override void Initialize(RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+            if (Session["peopleList"] == null)
+            {
+                Session["peopleList"] = people;
+            }
+        }
+        // GET: Personal
+        public ActionResult Index()
+        {
+            var p = (List<Person>)Session["peopleList"];
+            return View(p);
+        }
+
+        // GET: Personal/Details/5
+        public ActionResult Details(int id)
+        {
+            var pList = (List<Person>)Session["peopleList"];
+            var p = pList[id];
+            return View(p);
+        }
+
+        // GET: Personal/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Personal/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                people = (List<Person>)Session["peopleList"];
+                Person newPerson = new Person()
+                {
+                    id = people.Count(),
+                    firstname = collection["firstname"],
+                    middlename = collection["middlename"],
+                    lastname = collection["lastname"],
+                    cell = collection["cell"],
+                    relationship = collection["relationship"],
+                    familyId = int.Parse(collection["familyId"]
+                    )
+                };
+                people = (List<Person>)Session["peopleList"];
+                people.Add(newPerson);
+                Session["peopleList"] = people;
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Personal/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var pList = (List<Person>)Session["peopleList"];
+            var p = pList[id];
+            return View(p);
+        }
+
+        // POST: Personal/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                var pList = (List<Person>)Session["peopleList"];
+                var p = pList[id];
+
+                Person newPerson = new Person()
+                {
+                    id = id,
+                    firstname = collection["firstname"],
+                    middlename = collection["middlename"],
+                    lastname = collection["lastname"],
+                    cell = collection["cell"],
+                    relationship = collection["relationship"],
+                    familyId = int.Parse(collection["familyId"]
+                   )
+                };
+                pList.Where(x => x.id == id).First().firstname = collection["firstname"];
+                pList.Where(x => x.id == id).First().middlename = collection["middlename"];
+                pList.Where(x => x.id == id).First().lastname = collection["lastname"];
+                pList.Where(x => x.id == id).First().cell = collection["cell"];
+                pList.Where(x => x.id == id).First().relationship = collection["relationship"];
+                pList.Where(x => x.id == id).First().familyId = int.Parse(collection["familyId"]);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Personal/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var pList = (List<Person>)Session["peopleList"];
+            var p = pList[id];
+            return View(p);
+        }
+
+        // POST: Personal/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                var pList = (List<Person>)Session["peopleList"];
+                var p = pList[id];
+
+                Session["peopleList"] = pList.Where(x => x.id != id).ToList();
+                pList = (List<Person>)Session["peopleList"];
+                for (int x = id; x < pList.Count(); x++)
+                {
+                    if (pList[x] != null)
+                        pList[x].id = x;
+                }
+
+                return RedirectToAction("Index");
+
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
